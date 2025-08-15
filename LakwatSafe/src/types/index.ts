efe6050c08a4479f2812b14def2f6899b124d1be
chezca-v <franchezca.z.bnyd@gmail.com>
2025-08-15 23:18:14 +0800

    First import of the screens

---
+++ LakwatSafe/src/types/index.ts
@@ -0,0 +1,11 @@
+export interface NavigationParams {
+  Home: undefined;
+  Settings: undefined;
+  Splash: undefined;
+}
+
+export type AppStackParamList = {
+  Home: undefined;
+  Settings: undefined;
+  Splash: undefined;
+};
